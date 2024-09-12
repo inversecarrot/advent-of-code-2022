fbe1c44ceb63e3b717d90d1e87be6ef9e582dcf9
Sarah Trowbridge <trow.sar@gmail.com>
2024-09-11 21:03:06 -0700

    completed day 7

--- day7/day7.py
+++ day7/day7.py
@@ -1,16 +1,16 @@
 from filetree import FileTree
 
-def get_size(filename: str) -> str:
-    with open(filename, encoding='utf-8') as commands:
-        tree = FileTree(commands)
+def get_size(tree: FileTree) -> str:
     return tree.get_sum_of_large_directories()
 
-def part1(filename: str) -> str:
-    return get_size(filename)
+def part1(tree: FileTree) -> str:
+    return get_size(tree)
 
-def part2(filename: str) -> str:
-    return ''
+def part2(tree: FileTree) -> str:
+    return tree.get_smallest_big_directory(70000000, 30000000)
 
 if __name__ == '__main__':
-    print(part1('day7\\input.txt'))
-    print(part2('day7\\input.txt'))
+    with open('day7\\input.txt', encoding='utf-8') as commands:
+        command_tree = FileTree(commands)
+        print(part1(command_tree))
+        print(part2(command_tree))
--- day7/filetree.py
+++ day7/filetree.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from enum import Enum
 from io import TextIOWrapper
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 class NodeType(Enum):
     DIR = 0
@@ -25,8 +25,10 @@ class FileNode(Node):
 
 class FileTree:
     root: Node
+    dir_sizes: Dict[str, int]
     def __init__(self, commands: TextIOWrapper):
         self.root = DirectoryNode(NodeType.DIR, '/', None, [])
+        self.dir_sizes = {}
         next_command = commands.readline()
         cur = None
         while next_command:
@@ -58,7 +60,8 @@ class FileTree:
                         next_node = next((node for node in cur.children if node.name == tokens[2]))
                         cur = next_node
                         next_command = commands.readline()
-        # self.print_tree()
+        self._compute_directory_sizes(self.root)
+
 
     def get_sum_of_large_directories(self):
         return self._get_sum_of_large_directories(self.root)[1]
@@ -93,6 +96,26 @@ class FileTree:
             print(node_string + ')')
             for child in node.children:
                 self._print_node(child, depth + 1)
+
+    def get_smallest_big_directory(self, total_space: int, free_space: int):
+        current_space = total_space - self.dir_sizes[self.root.name]
+        min_size = free_space - current_space
+        smallest_dir = self.dir_sizes[self.root.name]
+        for val in self.dir_sizes.values():
+            if val >= min_size and val < smallest_dir:
+                smallest_dir = val
+        return smallest_dir
+    
+    def _compute_directory_sizes(self, node: Node) -> int:
+        if node.type == NodeType.FILE:
+            return int(node.size)
+        else:
+            size = 0
+            for child in node.children:
+                size += self._compute_directory_sizes(child)
+            self.dir_sizes[node.name] = size
+            return size
+
 
         
         
