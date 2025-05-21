7c26e3eed00d6e02b57a413a5388761efb840a53
Sarah Trowbridge <trow.sar@gmail.com>
2025-05-21 10:36:27 -0700

    fixed rope grid visualization bug

--- day9/rope_grid.py
+++ day9/rope_grid.py
@@ -9,24 +9,27 @@ class RopeGrid:
     rope: List[Tuple[int, int]]
     length: int
     dimensions: Tuple[int, int]
-    dimensions_set: bool
+    origin: Tuple[int, int]
     debug: bool
     def __init__(self, moves: List[Tuple[int, int]], length = 2, debug = False):
         self.moves = moves
-        self.rope = [(0,0)] * length
+        self._set_dimensions()
+        self.rope = [self.origin] * length
         self.length = length
-        self.dimensions_set = False
         self.debug = debug
     
     def get_tail_visits(self) -> int:
-        tail_positions = {(0,0)}
+        tail_positions = {self.origin}
+        last_move = (-1,-1)
         for move in self.moves:
+            if self.debug and move != last_move:
+                log.debug("\n" + self._get_visual_grid())
             self.rope[0] = tuple(sum(x) for x in zip(self.rope[0], move))
             for segment in range(1,len(self.rope)):
                 self.rope[segment] = self._get_tail_position(self.rope[segment - 1], self.rope[segment])
             tail_positions.add(self.rope[self.length - 1])
-            if (self.debug):
-                log.debug("\n" + self._get_visual_grid())
+            last_move = move
+            
         if (self.debug):
             log.debug(self._get_tail_visual_grid(tail_positions))
         return len(tail_positions)
@@ -57,21 +60,24 @@ class RopeGrid:
         return (cur_tail[0] + x_change, cur_tail[1] + y_change)
     
     def _set_dimensions(self):
-        max_x, max_y, cur_x, cur_y = [0,0,0,0]
+        # move assuming origin is at 0,0 and then recalibrate
+        max_x, max_y, min_x, min_y, cur_x, cur_y = [0,0,0,0,0,0]
         for move in self.moves:
             cur_x += move[0]
             cur_y += move[1]
             if cur_x > max_x:
                 max_x = cur_x
+            if cur_x < min_x:
+                min_x = cur_x
             if cur_y > max_y:
                 max_y = cur_y
-        self.dimensions = (max_x + 1, max_y + 1)
-        self.dimensions_set = True
+            if cur_y < min_y:
+                min_y = cur_y
+        self.dimensions = ((max_x + 1 - min_x), (max_y + 1 - min_y))
+        self.origin = (-1 * min_x, -1 * min_y)
 
 
     def _get_visual_grid(self) -> str:
-        if not self.dimensions_set:
-            self._set_dimensions()
         # indexed [y][x]
         matrix = [['.' for x in range(0,self.dimensions[0])] for y in range(0,self.dimensions[1])]
         for x in reversed(range(0, len(self.rope))):
@@ -82,7 +88,7 @@ class RopeGrid:
                 matrix[segment[1]][segment[0]] = f'{x}'
         
         output = '\n'
-        for row in matrix:
+        for row in reversed(matrix):
             for char in row:
                 output += char
             output += "\n"
@@ -90,15 +96,13 @@ class RopeGrid:
         return output
     
     def _get_tail_visual_grid(self, tail_positions) -> str:
-        if not self.dimensions_set:
-            self._set_dimensions()
         # indexed [y][x]
         matrix = [['.' for x in range(0,self.dimensions[0])] for y in range(0,self.dimensions[1])]
         for pos in tail_positions:
             matrix[pos[1]][pos[0]] = '#'
         
         output = '\n'
-        for row in matrix:
+        for row in reversed(matrix):
             for char in row:
                 output += char
             output += "\n"
